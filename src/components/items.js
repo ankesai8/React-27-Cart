4666f2d0562c8550bd8d08ff712c67e2d7b4a216
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-02-24 09:39:32 +0530

    Add files via upload

---
+++ src/components/items.js
@@ -0,0 +1,76 @@
+let items =[
+    {
+        id:1,
+        image:"./shop/fancy-products.png",
+        title: "OppO Product",
+        rating:"⭐⭐⭐⭐",
+        cost:" $140.00 - $180.00",
+        btn:"Add to cart",
+        text : false
+    },
+    {
+        id:2,
+        image:"./shop/special.jpg",
+        title: "OPPo MObile",
+        rating:"⭐⭐⭐⭐⭐",
+        cost:"$118.00",
+        btn:"Add to cart",
+        text : false
+    },
+    {
+        id:3,
+        image:"./shop/sale.jpg",
+        title: "MI",
+        rating:"⭐⭐⭐⭐",
+        cost:"$100.00 - $125.00",
+        btn:"Add to cart",
+        text : false
+    },
+    {
+        id:4,
+        image:"./shop/popular.jpg",
+        title: "Popular TV",
+        rating:"⭐⭐⭐⭐⭐",
+        cost:" $400.00",
+        btn:"Add to cart",
+        text : false
+    },
+    {
+        id:5,
+        image:"./shop/sale-2.jpg",
+        title: "Sale Item",
+        rating:"⭐⭐⭐⭐",
+        cost:" $500.00 - $2000.00",
+        btn:"Add to cart",
+        text : false
+    },
+    {
+        id:6,
+        image:"./shop/fancyy.jpg",
+        title: "Fancy Product",
+        rating:"⭐⭐⭐⭐",
+        cost:"$120.00 - $280.00",
+        btn:"View option",
+        text : false
+    },
+    {
+        id:7,
+        image:"./shop/special2.jpg",
+        title: "Special Item",
+        rating:"⭐⭐⭐⭐",
+        cost:" $20.00 - $18.00",
+        btn:"Add to cart",
+        text : false
+    },
+    {
+        id:8,
+        image:"./shop/popula.jpg",
+        title: "Popular Item",
+        rating:"⭐⭐⭐",
+        cost:"$40.00",
+        btn:"Add to cart",
+        text : false
+    }
+]
+
+export default items;
